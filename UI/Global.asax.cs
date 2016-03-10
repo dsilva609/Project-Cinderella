dbe22f370e1f6744b611d916bbba93901f7ab23e
dsilva609 <dsilva609@gmail.com>
2016-03-09 23:01:29 -0500

    removed some unnecessary elements and some refactoring

--- UI/Global.asax.cs
+++ UI/Global.asax.cs
@@ -1,28 +1,27 @@
-﻿using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using UI.App_Start;
 
 namespace UI
 {
-	public class MvcApplication : System.Web.HttpApplication
-	{
-		protected void Application_Start()
-		{
-			AreaRegistration.RegisterAllAreas();
-			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-			RouteConfig.RegisterRoutes(RouteTable.Routes);
-			BundleConfig.RegisterBundles(BundleTable.Bundles);
-			AutoMapperConfig.RegisterMappings();
-		}
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AutoMapperConfig.RegisterMappings();
+        }
 
-		protected void Session_Start()
-		{
-			HttpContext.Current.Session.Add("CardSortAscending", true);
-			HttpContext.Current.Session.Add("CardSortPreference", "Name");
-			HttpContext.Current.Session.Add("PlayerSortAscending", true);
-			HttpContext.Current.Session.Add("PlayerSortPreference", "Name");
-		}
-	}
+        protected void Session_Start()
+        {
+            //HttpContext.Current.Session.Add("CardSortAscending", true);
+            //HttpContext.Current.Session.Add("CardSortPreference", "Name");
+            //HttpContext.Current.Session.Add("PlayerSortAscending", true);
+            //HttpContext.Current.Session.Add("PlayerSortPreference", "Name");
+        }
+    }
 }
