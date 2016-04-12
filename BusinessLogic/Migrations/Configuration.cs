27feda6bcae181ecd4dfbe6bb6d5d73144b6db50
dsilva609 <dsilva609@gmail.com>
2016-04-12 19:07:44 -0400

    enabled migrations to run on app start

--- BusinessLogic/Migrations/Configuration.cs
+++ BusinessLogic/Migrations/Configuration.cs
@@ -1,28 +1,28 @@
 namespace BusinessLogic.Migrations
 {
-	using System.Data.Entity.Migrations;
+    using System.Data.Entity.Migrations;
 
-	internal sealed class Configuration : DbMigrationsConfiguration<BusinessLogic.DAL.ProjectCinderellaContext>
-	{
-		public Configuration()
-		{
-			AutomaticMigrationsEnabled = false;
-		}
+    public class Configuration : DbMigrationsConfiguration<DAL.ProjectCinderellaContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
 
-		protected override void Seed(BusinessLogic.DAL.ProjectCinderellaContext context)
-		{
-			//  This method will be called after migrating to the latest version.
+        protected override void Seed(DAL.ProjectCinderellaContext context)
+        {
+            //  This method will be called after migrating to the latest version.
 
-			//  You can use the DbSet<T>.AddOrUpdate() helper extension method
-			//  to avoid creating duplicate seed data. E.g.
-			//
-			//    context.People.AddOrUpdate(
-			//      p => p.FullName,
-			//      new Person { FullName = "Andrew Peters" },
-			//      new Person { FullName = "Brice Lambson" },
-			//      new Person { FullName = "Rowan Miller" }
-			//    );
-			//
-		}
-	}
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
 }
--- UI/Global.asax.cs
+++ UI/Global.asax.cs
@@ -1,26 +1,32 @@
-﻿using System.Web.Mvc;
+﻿using BusinessLogic.DAL;
+using System.Configuration;
+using System.Data.Entity.Migrations;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace UI
 {
-	public class MvcApplication : System.Web.HttpApplication
-	{
-		protected void Application_Start()
-		{
-			AreaRegistration.RegisterAllAreas();
-			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-			RouteConfig.RegisterRoutes(RouteTable.Routes);
-			BundleConfig.RegisterBundles(BundleTable.Bundles);
-			AutoMapperConfig.RegisterMappings();
-		}
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AutoMapperConfig.RegisterMappings();
 
-		protected void Session_Start()
-		{
-			//HttpContext.Current.Session.Add("CardSortAscending", true);
-			//HttpContext.Current.Session.Add("CardSortPreference", "Name");
-			//HttpContext.Current.Session.Add("PlayerSortAscending", true);
-			//HttpContext.Current.Session.Add("PlayerSortPreference", "Name");
-		}
-	}
+            var migrator = new DbMigrator(new BusinessLogic.Migrations.Configuration());
+            migrator.Update();
+        }
+
+        protected void Session_Start()
+        {
+            //HttpContext.Current.Session.Add("CardSortAscending", true);
+            //HttpContext.Current.Session.Add("CardSortPreference", "Name");
+            //HttpContext.Current.Session.Add("PlayerSortAscending", true);
+            //HttpContext.Current.Session.Add("PlayerSortPreference", "Name");
+        }
+    }
 }
