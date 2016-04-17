77c44c2d21ab2146f71dcb984d1ca4ae0ca5c230
dsilva609 <dsilva609@gmail.com>
2016-04-17 16:33:04 -0400

    added record label and album notes

--- BusinessLogic/Models/RecordModel.cs
+++ BusinessLogic/Models/RecordModel.cs
@@ -6,52 +6,53 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BusinessLogic.Models
 {
-    public class RecordModel
-    {
-        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int ID { get; set; }
+	public class RecordModel
+	{
+		public int ID { get; set; }
 
-        [Required]
-        public string UserID { get; set; }
+		[Required]
+		public string UserID { get; set; }
 
-        //[Key]
-        [Column(Order = 1)]
-        [Required]
-        [DisplayName("Album Name")]
-        public string AlbumName { get; set; }
+		[Column(Order = 1)]
+		[Required]
+		[DisplayName("Album Name")]
+		public string AlbumName { get; set; }
 
-        //[Key]
-        [Column(Order = 2)]
-        [Required]
-        public string Artist { get; set; }
+		[Column(Order = 2)]
+		[Required]
+		public string Artist { get; set; }
 
-        public string Genre { get; set; }
+		public string Genre { get; set; }
 
-        [DisplayName("Album Year")]
-        public int AlbumYear { get; set; }
+		[DisplayName("Album Year")]
+		public int AlbumYear { get; set; }
 
-        //[Key]
-        [Column(Order = 3)]
-        [Required]
-        [DisplayName("Media Type")]
-        public MediaTypeEnum MediaType { get; set; }
+		[Column(Order = 3)]
+		[Required]
+		[DisplayName("Media Type")]
+		public MediaTypeEnum MediaType { get; set; }
 
-        [Required]
-        [DisplayName("Physical?")]
-        public bool IsPhysical { get; set; }
+		[Required]
+		[DisplayName("Physical?")]
+		public bool IsPhysical { get; set; }
 
-        [Required]
-        [DisplayName("New?")]
-        public bool IsNew { get; set; }
+		[Required]
+		[DisplayName("Purchased New?")]
+		public bool IsNew { get; set; }
 
-        [DisplayName("Location Purchased")]
-        public string LocationPurchased { get; set; }
+		[DisplayName("Location Purchased")]
+		public string LocationPurchased { get; set; }
 
-        [DisplayName("Date Purchased")]
-        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
-        public DateTime DatePurchased { get; set; } = DateTime.Now;
+		[DisplayName("Date Purchased")]
+		[DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+		public DateTime DatePurchased { get; set; } = DateTime.Today;
 
-        public DateTime DateAdded { get; set; }
-        public DateTime DateUpdated { get; set; } = Convert.ToDateTime("1/1/1900");
-    }
+		public DateTime DateAdded { get; set; }
+		public DateTime DateUpdated { get; set; } = Convert.ToDateTime("1/1/1900");
+
+		public string Notes { get; set; }
+
+		[DisplayName("Record Label")]
+		public string RecordLabel { get; set; }
+	}
 }
