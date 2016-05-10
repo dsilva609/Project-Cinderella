8d002fbca14061f3f3949f23d6732b736e915487
dsilva609 <dsilva609@gmail.com>
2016-05-10 13:32:55 -0400

    created movie service

---
+++ BusinessLogic/Services/Interfaces/IMovieService.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLogic.Models;
+using System.Collections.Generic;
+
+namespace BusinessLogic.Services.Interfaces
+{
+	public interface IMovieService
+	{
+		void Add(Movie movie);
+
+		List<Movie> GetAll(string userID = "", string query = "");//, int numToTake = 0, int? pageNum = 1);
+
+		Movie GetByID(int id, string userID);
+
+		void Edit(int id, Movie movie);
+
+		void Delete(int id, string userID);
+
+		int GetCount();
+	}
+}
---
+++ BusinessLogic/Services/MovieService.cs
@@ -0,0 +1,40 @@
+﻿using BusinessLogic.Models;
+using BusinessLogic.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace BusinessLogic.Services
+{
+	public class MovieService : IMovieService
+	{
+		public void Add(Movie movie)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Movie> GetAll(string userID = "", string query = "")
+		{
+			throw new NotImplementedException();
+		}
+
+		public Movie GetByID(int id, string userID)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Edit(int id, Movie movie)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Delete(int id, string userID)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int GetCount()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
