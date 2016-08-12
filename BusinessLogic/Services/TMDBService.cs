7e1e4974acdd4a6b81bb9b3245ab8cdcc60301bd
Dennis D. Silva <dsilva609@gmail.com>
2016-08-12 12:41:19 -0400

    started implementing tv show support

--- BusinessLogic/Services/TMDBService.cs
+++ BusinessLogic/Services/TMDBService.cs
@@ -12,58 +12,82 @@ using System.Net.Http.Headers;
 
 namespace BusinessLogic.Services
 {
-	public class TMDBService : ITMDBService
-	{
-		private HttpClient _client;
+    public class TMDBService : ITMDBService
+    {
+        private HttpClient _client;
 
-		public TMDBService()
-		{
-			CreateClient();
-		}
+        public TMDBService()
+        {
+            CreateClient();
+        }
 
-		public List<TMDBMovie> SearchMovies(string title)
-		{
-			var response = _client.GetAsync($"search/movie?api_key={Settings.Default.TMDBKey}&query={title}");
+        public List<TMDBMovie> SearchMovies(string title)
+        {
+            var response = _client.GetAsync($"search/movie?api_key={Settings.Default.TMDBKey}&query={title}");
 
-			var result = response.Result.Content.ReadAsStringAsync().Result;
-			var movies = JsonConvert.DeserializeObject<TMDBMovieResult>(result);
+            var result = response.Result.Content.ReadAsStringAsync().Result;
+            var movies = JsonConvert.DeserializeObject<TMDBMovieResult>(result);
 
-			return movies.results;
-		}
+            return movies.results;
+        }
 
-		public Movie SearchMovieByID(int id)
-		{
-			var response = _client.GetAsync($"movie/{id}?api_key={Settings.Default.TMDBKey}");
+        public Movie SearchMovieByID(int id)
+        {
+            var response = _client.GetAsync($"movie/{id}?api_key={Settings.Default.TMDBKey}");
 
-			var result = response.Result.Content.ReadAsStringAsync().Result;
-			var tmdbMovie = JsonConvert.DeserializeObject<TMDBMovie>(result);
-			var movie = ConvertTMDDMovieToMovie(tmdbMovie);
+            var result = response.Result.Content.ReadAsStringAsync().Result;
+            var tmdbMovie = JsonConvert.DeserializeObject<TMDBMovie>(result);
+            var movie = ConvertTMDDMovieToMovie(tmdbMovie);
 
-			return movie;
-		}
+            return movie;
+        }
 
-		private void CreateClient()
-		{
-			_client = new HttpClient { BaseAddress = new Uri("https://api.themoviedb.org/3/") };
-			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
-			_client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-			_client.DefaultRequestHeaders.Add("User-Agent", "Project-Cinderella/1.0 +projectcinderella.azurewebsites.net");
-		}
+        //TODO: refactor
+        public List<TMDBMovie> SearchTV(string title)
+        {
+            var response = _client.GetAsync($"search/tv?api_key={Settings.Default.TMDBKey}&query={title}");
 
-		private Movie ConvertTMDDMovieToMovie(TMDBMovie tmdb)
-		{
-			var movie = new Movie
-			{
-				Title = tmdb.title,
-				Distributor = tmdb.production_companies.First().name,
-				Genre = string.Join(", ", tmdb.genres.Select(x => x.name).ToList()),
-				ImageUrl = string.Format("https://image.tmdb.org/t/p/w500{0}", tmdb.poster_path),
-				Language = tmdb.original_language,
-				TMDBID = tmdb.id,
-				YearReleased = DateTime.Parse(tmdb.release_date).Year
-			};
+            var result = response.Result.Content.ReadAsStringAsync().Result;
+            var movies = JsonConvert.DeserializeObject<TMDBMovieResult>(result);
 
-			return movie;
-		}
-	}
+            return movies.results;
+        }
+
+        //TODO: refactor
+        public Movie SearchTVShowByID(int id)
+        {
+            var response = _client.GetAsync($"movie/{id}?api_key={Settings.Default.TMDBKey}");
+
+            var result = response.Result.Content.ReadAsStringAsync().Result;
+            var tmdbMovie = JsonConvert.DeserializeObject<TMDBMovie>(result);
+            //TODO: create separate method for tv shows
+            var movie = ConvertTMDDMovieToMovie(tmdbMovie);
+
+            return movie;
+        }
+
+        private void CreateClient()
+        {
+            _client = new HttpClient { BaseAddress = new Uri("https://api.themoviedb.org/3/") };
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _client.DefaultRequestHeaders.Add("User-Agent", "Project-Cinderella/1.0 +projectcinderella.azurewebsites.net");
+        }
+
+        private Movie ConvertTMDDMovieToMovie(TMDBMovie tmdb)
+        {
+            var movie = new Movie
+            {
+                Title = tmdb.title,
+                Distributor = tmdb.production_companies.First().name,
+                Genre = string.Join(", ", tmdb.genres.Select(x => x.name).ToList()),
+                ImageUrl = string.Format("https://image.tmdb.org/t/p/w500{0}", tmdb.poster_path),
+                Language = tmdb.original_language,
+                TMDBID = tmdb.id,
+                YearReleased = DateTime.Parse(tmdb.release_date).Year
+            };
+
+            return movie;
+        }
+    }
 }
