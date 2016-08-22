5d1eee0479877fbf1fc50e168f6922537e017869
Dennis D. Silva <dsilva609@gmail.com>
2016-08-22 12:26:33 -0400

    added attributions and logos

--- UI/T4MVC.cs
+++ UI/T4MVC.cs
@@ -362,7 +362,11 @@ namespace Links
             public static string Url(string fileName) { return T4MVCHelpers.ProcessVirtualPath(URLPATH + "/" + fileName); }
             public static readonly string ajax_loader_gif = Url("ajax-loader.gif");
             public static readonly string Book_jpg = Url("Book.jpg");
+            public static readonly string Discogs_Logo_png = Url("Discogs.Logo.png");
             public static readonly string Game_jpg = Url("Game.jpg");
+            public static readonly string Geek_Head_jpg = Url("Geek_Head.jpg");
+            public static readonly string GiantBombLogo_jpg = Url("GiantBombLogo.jpg");
+            public static readonly string GoogleBooksLogo_png = Url("GoogleBooksLogo.png");
             [GeneratedCode("T4MVC", "2.0"), DebuggerNonUserCode]
             public static class icons_png {
                 private const string URLPATH = "~/Content/Images/icons-png";
