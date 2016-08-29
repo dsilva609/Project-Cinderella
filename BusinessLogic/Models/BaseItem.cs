4179194122629ce64d17dbfb48929c246bb2944c
dsilva609 <dsilva609@gmail.com>
2016-08-29 19:26:07 -0400

    fixed enum display helper and updated enum displays

--- BusinessLogic/Models/BaseItem.cs
+++ BusinessLogic/Models/BaseItem.cs
@@ -5,44 +5,45 @@ using System.ComponentModel.DataAnnotations;
 
 namespace BusinessLogic.Models
 {
-    public class BaseItem
-    {
-        [Key]
-        public int ID { get; set; }
+	public class BaseItem
+	{
+		[Key]
+		public int ID { get; set; }
 
-        [Required]
-        public string UserID { get; set; }
+		[Required]
+		public string UserID { get; set; }
 
-        [Required]
-        public string Title { get; set; }
+		[Required]
+		public string Title { get; set; }
 
-        public string Genre { get; set; }
-        public string Language { get; set; }
-        public string Notes { get; set; }
+		public string Genre { get; set; }
+		public string Language { get; set; }
+		public string Notes { get; set; }
 
-        [DisplayName("Image Url")]
-        public string ImageUrl { get; set; }
+		[DisplayName("Image Url")]
+		public string ImageUrl { get; set; }
 
-        [DisplayName("Year Released")]
-        public int YearReleased { get; set; } = DateTime.Today.Year;
+		[DisplayName("Year Released")]
+		public int YearReleased { get; set; } = DateTime.Today.Year;
 
-        [Required]
-        [DisplayName("Physical?")]
-        public bool IsPhysical { get; set; }
+		[Required]
+		[DisplayName("Physical?")]
+		public bool IsPhysical { get; set; }
 
-        [Display(Name = "Purchased New?")]
-        public bool IsNew { get; set; }
+		[Display(Name = "Purchased New?")]
+		public bool IsNew { get; set; }
 
-        [DisplayName("Location Purchased")]
-        public string LocationPurchased { get; set; }
+		[DisplayName("Location Purchased")]
+		public string LocationPurchased { get; set; }
 
-        [Display(Name = "Date Purchased")]
-        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
-        public DateTime DatePurchased { get; set; } = DateTime.Today;
+		[Display(Name = "Date Purchased")]
+		[DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+		public DateTime DatePurchased { get; set; } = DateTime.Today;
 
-        public DateTime DateAdded { get; set; }
-        public DateTime DateUpdated { get; set; } = Convert.ToDateTime("1/1/1900");
+		public DateTime DateAdded { get; set; }
+		public DateTime DateUpdated { get; set; } = Convert.ToDateTime("1/1/1900");
 
-        public CompletionStatus CompletionStatus { get; set; }
-    }
+		[DisplayName("Completion Status")]
+		public CompletionStatus CompletionStatus { get; set; }
+	}
 }
--- UI/Enums/EnumHelpers.cs
+++ UI/Enums/EnumHelpers.cs
@@ -3,17 +3,15 @@ using System.ComponentModel.DataAnnotations;
 
 namespace UI.Enums
 {
-    public static class EnumHelpers
-    {
-        public static string GetDisplayName(this Enum value)
-        {
-            var type = value.GetType();
-            var name = Enum.GetName(type, value);
-            if (name == null) return null;
-            var field = type.GetField(name);
-            if (field == null) return null;
-            var attr = Attribute.GetCustomAttribute(field, typeof(DisplayAttribute)) as DisplayAttribute;
-            return attr?.Name;
-        }
-    }
+	public static class EnumHelpers
+	{
+		public static string GetDisplayName(this Enum value)
+		{
+			var type = value.GetType();
+			var name = Enum.GetName(type, value);
+			var field = type.GetField(name);
+			var attr = Attribute.GetCustomAttribute(field, typeof(DisplayAttribute)) as DisplayAttribute;
+			return attr?.Name ?? value.ToString();
+		}
+	}
 }
