3017e138b0c6e6f9b5f541fd0fba6e90e1b6b62f
Dennis D. Silva <dsilva609@gmail.com>
2016-09-06 12:25:31 -0400

    started adding status helper to album

---
+++ UI/Enums/CompletionStatusHelper.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLogic.Enums;
+
+namespace UI.Enums
+{
+    public static class CompletionStatusHelper
+    {
+        public static string GetStatusClass(CompletionStatus status)
+        {
+            var statusClass = "fa ";
+            switch (status)
+            {
+                case CompletionStatus.NotStarted:
+                    statusClass += "fa-circle";
+                    break;
+
+                case CompletionStatus.InProgress:
+                    statusClass += "fa-spinner";
+                    break;
+
+                case CompletionStatus.Completed:
+                    statusClass += "fa-check-circle";
+                    break;
+            }
+
+            return statusClass;
+        }
+    }
+}
--- UI/Enums/EnumHelpers.cs
+++ UI/Enums/EnumHelpers.cs
@@ -3,15 +3,15 @@ using System.ComponentModel.DataAnnotations;
 
 namespace UI.Enums
 {
-	public static class EnumHelpers
-	{
-		public static string GetDisplayName(this Enum value)
-		{
-			var type = value.GetType();
-			var name = Enum.GetName(type, value);
-			var field = type.GetField(name);
-			var attr = Attribute.GetCustomAttribute(field, typeof(DisplayAttribute)) as DisplayAttribute;
-			return attr?.Name ?? value.ToString();
-		}
-	}
+    public static class EnumHelpers
+    {
+        public static string GetDisplayName(this Enum value)
+        {
+            var type = value.GetType();
+            var name = Enum.GetName(type, value);
+            var field = type.GetField(name);
+            var attr = Attribute.GetCustomAttribute(field, typeof(DisplayAttribute)) as DisplayAttribute;
+            return attr?.Name ?? value.ToString();
+        }
+    }
 }
