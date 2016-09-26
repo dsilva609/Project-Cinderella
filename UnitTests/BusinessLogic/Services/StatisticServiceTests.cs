2203ff7606116a48614329ad917115af6e2de43f
Dennis D. Silva <dsilva609@gmail.com>
2016-09-26 13:09:12 -0400

    started tests for statistic service

---
+++ UnitTests/BusinessLogic/Services/StatisticServiceTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using UnitTests.BusinessLogic.Services.TestBases;
+
+namespace UnitTests.BusinessLogic.Services
+{
+    public class StatisticServiceTests : StatisticServiceTestBase
+    {
+        [Test]
+        public void ThatItReturnsGlobalCollectionCount()
+        {
+            0.ShouldBe(1);
+        }
+    }
+}
---
+++ UnitTests/BusinessLogic/Services/TestBases/StatisticServiceTestBase.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLogic.Services.Interfaces;
+using NUnit.Framework;
+using StructureMap.AutoMocking;
+
+namespace UnitTests.BusinessLogic.Services.TestBases
+{
+    public class StatisticServiceTestBase
+    {
+        protected RhinoAutoMocker<IStatisticService> _service;
+
+        [SetUp]
+        public virtual void SetUp()
+        {
+            _service = new RhinoAutoMocker<IStatisticService>();
+        }
+    }
+}
