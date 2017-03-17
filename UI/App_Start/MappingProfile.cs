98c4faf1277a0f03a3b046d68f913b8cbdbd514a
Dennis D. Silva <dsilva609@gmail.com>
2017-03-17 13:59:37 -0400

    started adding auto mapper

---
+++ UI/App_Start/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using UI.MappingProfiles;
+
+namespace UI
+{
+    public static class MappingProfile
+    {
+        public static MapperConfiguration InitializeAutoMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new AlbumViewModelProfile());
+                //cfg.CreateMap<Question, QuestionModel>();
+                //cfg.CreateMap<QuestionModel, Question>();
+                /*etc...*/
+            });
+
+            return config;
+        }
+    }
+}
---
+++ UI/MappingProfiles/AlbumViewModelProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BusinessLogic.Models;
+using UI.Models;
+
+namespace UI.MappingProfiles
+{
+    public class AlbumViewModelProfile : Profile
+    {
+        public AlbumViewModelProfile()
+        {
+            CreateMap<Album, ItemViewModel>();
+        }
+    }
+}
