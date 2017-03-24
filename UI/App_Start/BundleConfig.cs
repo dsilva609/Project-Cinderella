e0677463dbfc8cd242256d344969be9099144bc4
Dennis D. Silva <dsilva609@gmail.com>
2017-03-24 13:04:06 -0400

    started adding a select/textbox for categories

--- UI/App_Start/BundleConfig.cs
+++ UI/App_Start/BundleConfig.cs
@@ -2,51 +2,55 @@
 
 namespace UI
 {
-	public class BundleConfig
-	{
-		// For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
-		public static void RegisterBundles(BundleCollection bundles)
-		{
-			bundles.Add(new ScriptBundle("~/bundles/jquery")
-				.Include("~/Scripts/jquery-{version}.min.js"));
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
+                .Include("~/Scripts/jquery-{version}.min.js"));
 
-			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-				"~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                "~/Scripts/jquery.validate*"));
 
-			// Use the development version of Modernizr to develop with and learn from. Then, when you're
-			// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-				"~/Scripts/modernizr-*"));
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                "~/Scripts/modernizr-*"));
 
-			bundles.Add(new ScriptBundle("~/bundles/bootstrap")
-				.Include("~/Scripts/bootstrap-{version}.min.js")
-				.Include("~/Scripts/bootstrap-3.0.1.min.js")
-				.Include("~/Scripts/respond.min.js")
-				.Include("~/Scripts/bootstrap-datepicker.min.js")
-				.Include("~/Scripts/tether/tether.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap")
+                    .Include("~/Scripts/bootstrap-{version}.min.js")
+                    .Include("~/Scripts/bootstrap-3.0.1.min.js")
+                    .Include("~/Scripts/respond.min.js")
+                    .Include("~/Scripts/bootstrap-datepicker.min.js")
+                    .Include("~/Scripts/tether/tether.min.js")
+            //.Include("~/Scripts/select2.min.js")
+            );
 
-			bundles.Add(new ScriptBundle("~/bundles/custom")
-				.Include("~/Scripts/Namespace.min.js")
-				.Include("~/Scripts/Shared/_Layout.min.js")
-				.Include("~/Scripts/Shared/Logout.min.js")
-				.Include("~/Scripts/toastr.min.js")
-				.Include("~/Scripts/moment.min.js")
-				.Include("~/Scripts/livestamp.min.js")
-				.Include("~/Scripts/flipclock.min.js")
-				.Include("~/Scripts/jquery.bcSwipe.min.js")
-				.Include("~/Scripts/PageSpecific/AlbumEdit.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/custom")
+                .Include("~/Scripts/Namespace.min.js")
+                .Include("~/Scripts/Shared/_Layout.min.js")
+                .Include("~/Scripts/Shared/Logout.min.js")
+                .Include("~/Scripts/toastr.min.js")
+                .Include("~/Scripts/moment.min.js")
+                .Include("~/Scripts/livestamp.min.js")
+                .Include("~/Scripts/flipclock.min.js")
+                .Include("~/Scripts/jquery.bcSwipe.min.js")
+                .Include("~/Scripts/PageSpecific/AlbumEdit.min.js"));
 
-			bundles.Add(new StyleBundle("~/Content/css")
-				.Include("~/Content/bootstrap/bootstrap.min.css")
-				.Include("~/Content/bootstrap/bootstrap-datepicker3.min.css")
-				.Include("~/Content/Site.min.css")
-				.Include("~/Content/toastr.min.css")
-				.Include("~/Content/font-awesome.min.css")
-				.Include("~/Content/flipclock.min.css"));
+            bundles.Add(new StyleBundle("~/Content/css")
+                    .Include("~/Content/bootstrap/bootstrap.min.css")
+                    .Include("~/Content/bootstrap/bootstrap-datepicker3.min.css")
+                    .Include("~/Content/Site.min.css")
+                    .Include("~/Content/toastr.min.css")
+                    .Include("~/Content/font-awesome.min.css")
+                    .Include("~/Content/flipclock.min.css")
+            //.Include("~/Content/css/select2.min.css")
+            );
 
-			// Set EnableOptimizations to false for debugging. For more information,
-			// visit http://go.microsoft.com/fwlink/?LinkId=301862
-			BundleTable.EnableOptimizations = true;
-		}
-	}
+            // Set EnableOptimizations to false for debugging. For more information,
+            // visit http://go.microsoft.com/fwlink/?LinkId=301862
+            BundleTable.EnableOptimizations = true;
+        }
+    }
 }
--- UI/T4MVC.cs
+++ UI/T4MVC.cs
@@ -126,6 +126,60 @@ namespace Links
             public static readonly string salvattore_min_js = Url("salvattore.min.js");
         }
     
+        [GeneratedCode("T4MVC", "2.0"), DebuggerNonUserCode]
+        public static class i18n {
+            public const string UrlPath = "~/Scripts/i18n";
+            public static string Url() { return T4MVCHelpers.ProcessVirtualPath(UrlPath); }
+            public static string Url(string fileName) { return T4MVCHelpers.ProcessVirtualPath(UrlPath + "/" + fileName); }
+            public static readonly string ar_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/ar.min.js") ? Url("ar.min.js") : Url("ar.js");
+            public static readonly string az_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/az.min.js") ? Url("az.min.js") : Url("az.js");
+            public static readonly string bg_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/bg.min.js") ? Url("bg.min.js") : Url("bg.js");
+            public static readonly string ca_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/ca.min.js") ? Url("ca.min.js") : Url("ca.js");
+            public static readonly string cs_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/cs.min.js") ? Url("cs.min.js") : Url("cs.js");
+            public static readonly string da_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/da.min.js") ? Url("da.min.js") : Url("da.js");
+            public static readonly string de_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/de.min.js") ? Url("de.min.js") : Url("de.js");
+            public static readonly string el_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/el.min.js") ? Url("el.min.js") : Url("el.js");
+            public static readonly string en_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/en.min.js") ? Url("en.min.js") : Url("en.js");
+            public static readonly string es_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/es.min.js") ? Url("es.min.js") : Url("es.js");
+            public static readonly string et_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/et.min.js") ? Url("et.min.js") : Url("et.js");
+            public static readonly string eu_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/eu.min.js") ? Url("eu.min.js") : Url("eu.js");
+            public static readonly string fa_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/fa.min.js") ? Url("fa.min.js") : Url("fa.js");
+            public static readonly string fi_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/fi.min.js") ? Url("fi.min.js") : Url("fi.js");
+            public static readonly string fr_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/fr.min.js") ? Url("fr.min.js") : Url("fr.js");
+            public static readonly string gl_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/gl.min.js") ? Url("gl.min.js") : Url("gl.js");
+            public static readonly string he_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/he.min.js") ? Url("he.min.js") : Url("he.js");
+            public static readonly string hi_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/hi.min.js") ? Url("hi.min.js") : Url("hi.js");
+            public static readonly string hr_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/hr.min.js") ? Url("hr.min.js") : Url("hr.js");
+            public static readonly string hu_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/hu.min.js") ? Url("hu.min.js") : Url("hu.js");
+            public static readonly string id_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/id.min.js") ? Url("id.min.js") : Url("id.js");
+            public static readonly string is_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/is.min.js") ? Url("is.min.js") : Url("is.js");
+            public static readonly string it_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/it.min.js") ? Url("it.min.js") : Url("it.js");
+            public static readonly string ja_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/ja.min.js") ? Url("ja.min.js") : Url("ja.js");
+            public static readonly string km_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/km.min.js") ? Url("km.min.js") : Url("km.js");
+            public static readonly string ko_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/ko.min.js") ? Url("ko.min.js") : Url("ko.js");
+            public static readonly string lt_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/lt.min.js") ? Url("lt.min.js") : Url("lt.js");
+            public static readonly string lv_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/lv.min.js") ? Url("lv.min.js") : Url("lv.js");
+            public static readonly string mk_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/mk.min.js") ? Url("mk.min.js") : Url("mk.js");
+            public static readonly string ms_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/ms.min.js") ? Url("ms.min.js") : Url("ms.js");
+            public static readonly string nb_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/nb.min.js") ? Url("nb.min.js") : Url("nb.js");
+            public static readonly string nl_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/nl.min.js") ? Url("nl.min.js") : Url("nl.js");
+            public static readonly string pl_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/pl.min.js") ? Url("pl.min.js") : Url("pl.js");
+            public static readonly string pt_BR_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/pt-BR.min.js") ? Url("pt-BR.min.js") : Url("pt-BR.js");
+            public static readonly string pt_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/pt.min.js") ? Url("pt.min.js") : Url("pt.js");
+            public static readonly string ro_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/ro.min.js") ? Url("ro.min.js") : Url("ro.js");
+            public static readonly string ru_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/ru.min.js") ? Url("ru.min.js") : Url("ru.js");
+            public static readonly string sk_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/sk.min.js") ? Url("sk.min.js") : Url("sk.js");
+            public static readonly string sr_Cyrl_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/sr-Cyrl.min.js") ? Url("sr-Cyrl.min.js") : Url("sr-Cyrl.js");
+            public static readonly string sr_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/sr.min.js") ? Url("sr.min.js") : Url("sr.js");
+            public static readonly string sv_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/sv.min.js") ? Url("sv.min.js") : Url("sv.js");
+            public static readonly string th_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/th.min.js") ? Url("th.min.js") : Url("th.js");
+            public static readonly string tr_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/tr.min.js") ? Url("tr.min.js") : Url("tr.js");
+            public static readonly string uk_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/uk.min.js") ? Url("uk.min.js") : Url("uk.js");
+            public static readonly string vi_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/vi.min.js") ? Url("vi.min.js") : Url("vi.js");
+            public static readonly string zh_CN_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/zh-CN.min.js") ? Url("zh-CN.min.js") : Url("zh-CN.js");
+            public static readonly string zh_TW_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/zh-TW.min.js") ? Url("zh-TW.min.js") : Url("zh-TW.js");
+        }
+    
         public static readonly string jquery_3_1_1_intellisense_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/jquery-3.1.1.intellisense.min.js") ? Url("jquery-3.1.1.intellisense.min.js") : Url("jquery-3.1.1.intellisense.js");
         public static readonly string jquery_3_1_1_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/jquery-3.1.1.min.js") ? Url("jquery-3.1.1.min.js") : Url("jquery-3.1.1.js");
         public static readonly string jquery_3_1_1_min_js = Url("jquery-3.1.1.min.js");
@@ -248,6 +302,8 @@ namespace Links
         public static readonly string respond_matchmedia_addListener_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/respond.matchmedia.addListener.min.js") ? Url("respond.matchmedia.addListener.min.js") : Url("respond.matchmedia.addListener.js");
         public static readonly string respond_matchmedia_addListener_min_js = Url("respond.matchmedia.addListener.min.js");
         public static readonly string respond_matchmedia_addListener_min_js_map = Url("respond.matchmedia.addListener.min.js.map");
+        public static readonly string select2_js = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/select2.min.js") ? Url("select2.min.js") : Url("select2.js");
+        public static readonly string select2_min_js = Url("select2.min.js");
         [GeneratedCode("T4MVC", "2.0"), DebuggerNonUserCode]
         public static class Shared {
             public const string UrlPath = "~/Scripts/Shared";
@@ -400,6 +456,15 @@ namespace Links
         public static readonly string bootstrap_min_css_map = Url("bootstrap.min.css.map");
         public static readonly string c3_css = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/c3.min.css") ? Url("c3.min.css") : Url("c3.css");
         public static readonly string c3_min_css = Url("c3.min.css");
+        [GeneratedCode("T4MVC", "2.0"), DebuggerNonUserCode]
+        public static class css {
+            public const string UrlPath = "~/Content/css";
+            public static string Url() { return T4MVCHelpers.ProcessVirtualPath(UrlPath); }
+            public static string Url(string fileName) { return T4MVCHelpers.ProcessVirtualPath(UrlPath + "/" + fileName); }
+            public static readonly string select2_css = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/select2.min.css") ? Url("select2.min.css") : Url("select2.css");
+            public static readonly string select2_min_css = Url("select2.min.css");
+        }
+    
         public static readonly string flipclock_css = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/flipclock.min.css") ? Url("flipclock.min.css") : Url("flipclock.css");
         public static readonly string flipclock_min_css = Url("flipclock.min.css");
         public static readonly string font_awesome_css = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(UrlPath + "/font-awesome.min.css") ? Url("font-awesome.min.css") : Url("font-awesome.css");
@@ -574,6 +639,59 @@ namespace Links
                     public const string salvattore_min_js = "~/Scripts/Hydrogen/salvattore.min.js"; 
                 }
             }
+            public static partial class i18n 
+            {
+                public static class Assets
+                {
+                    public const string ar_js = "~/Scripts/i18n/ar.js"; 
+                    public const string az_js = "~/Scripts/i18n/az.js"; 
+                    public const string bg_js = "~/Scripts/i18n/bg.js"; 
+                    public const string ca_js = "~/Scripts/i18n/ca.js"; 
+                    public const string cs_js = "~/Scripts/i18n/cs.js"; 
+                    public const string da_js = "~/Scripts/i18n/da.js"; 
+                    public const string de_js = "~/Scripts/i18n/de.js"; 
+                    public const string el_js = "~/Scripts/i18n/el.js"; 
+                    public const string en_js = "~/Scripts/i18n/en.js"; 
+                    public const string es_js = "~/Scripts/i18n/es.js"; 
+                    public const string et_js = "~/Scripts/i18n/et.js"; 
+                    public const string eu_js = "~/Scripts/i18n/eu.js"; 
+                    public const string fa_js = "~/Scripts/i18n/fa.js"; 
+                    public const string fi_js = "~/Scripts/i18n/fi.js"; 
+                    public const string fr_js = "~/Scripts/i18n/fr.js"; 
+                    public const string gl_js = "~/Scripts/i18n/gl.js"; 
+                    public const string he_js = "~/Scripts/i18n/he.js"; 
+                    public const string hi_js = "~/Scripts/i18n/hi.js"; 
+                    public const string hr_js = "~/Scripts/i18n/hr.js"; 
+                    public const string hu_js = "~/Scripts/i18n/hu.js"; 
+                    public const string id_js = "~/Scripts/i18n/id.js"; 
+                    public const string is_js = "~/Scripts/i18n/is.js"; 
+                    public const string it_js = "~/Scripts/i18n/it.js"; 
+                    public const string ja_js = "~/Scripts/i18n/ja.js"; 
+                    public const string km_js = "~/Scripts/i18n/km.js"; 
+                    public const string ko_js = "~/Scripts/i18n/ko.js"; 
+                    public const string lt_js = "~/Scripts/i18n/lt.js"; 
+                    public const string lv_js = "~/Scripts/i18n/lv.js"; 
+                    public const string mk_js = "~/Scripts/i18n/mk.js"; 
+                    public const string ms_js = "~/Scripts/i18n/ms.js"; 
+                    public const string nb_js = "~/Scripts/i18n/nb.js"; 
+                    public const string nl_js = "~/Scripts/i18n/nl.js"; 
+                    public const string pl_js = "~/Scripts/i18n/pl.js"; 
+                    public const string pt_BR_js = "~/Scripts/i18n/pt-BR.js"; 
+                    public const string pt_js = "~/Scripts/i18n/pt.js"; 
+                    public const string ro_js = "~/Scripts/i18n/ro.js"; 
+                    public const string ru_js = "~/Scripts/i18n/ru.js"; 
+                    public const string sk_js = "~/Scripts/i18n/sk.js"; 
+                    public const string sr_Cyrl_js = "~/Scripts/i18n/sr-Cyrl.js"; 
+                    public const string sr_js = "~/Scripts/i18n/sr.js"; 
+                    public const string sv_js = "~/Scripts/i18n/sv.js"; 
+                    public const string th_js = "~/Scripts/i18n/th.js"; 
+                    public const string tr_js = "~/Scripts/i18n/tr.js"; 
+                    public const string uk_js = "~/Scripts/i18n/uk.js"; 
+                    public const string vi_js = "~/Scripts/i18n/vi.js"; 
+                    public const string zh_CN_js = "~/Scripts/i18n/zh-CN.js"; 
+                    public const string zh_TW_js = "~/Scripts/i18n/zh-TW.js"; 
+                }
+            }
             public static partial class locales 
             {
                 public static class Assets
@@ -706,6 +824,8 @@ namespace Links
                 public const string respond_js = "~/Scripts/respond.js"; 
                 public const string respond_matchmedia_addListener_js = "~/Scripts/respond.matchmedia.addListener.js"; 
                 public const string respond_matchmedia_addListener_min_js = "~/Scripts/respond.matchmedia.addListener.min.js"; 
+                public const string select2_js = "~/Scripts/select2.js"; 
+                public const string select2_min_js = "~/Scripts/select2.min.js"; 
                 public const string toastr_js = "~/Scripts/toastr.js"; 
                 public const string toastr_min_js = "~/Scripts/toastr.min.js"; 
             }
@@ -731,6 +851,14 @@ namespace Links
                     public const string bootstrap_datepicker3_standalone_css = "~/Content/bootstrap/bootstrap-datepicker3.standalone.css";
                     public const string bootstrap_datepicker3_standalone_min_css = "~/Content/bootstrap/bootstrap-datepicker3.standalone.min.css";
                     public const string bootstrap_css = "~/Content/bootstrap/bootstrap.css";
+                }
+            }
+            public static partial class css 
+            {
+                public static class Assets
+                {
+                    public const string select2_css = "~/Content/css/select2.css";
+                    public const string select2_min_css = "~/Content/css/select2.min.css";
                 }
             }
             public static partial class fonts 
