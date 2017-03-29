549d8ed9458d9ad62a9e5c9f88711c8cbdee2fb1
dsilva609 <dsilva609@gmail.com>
2017-03-28 22:44:12 -0400

    updated unit tests

--- UnitTests/UI/Controllers/HomeControllerTests.cs
+++ UnitTests/UI/Controllers/HomeControllerTests.cs
@@ -10,64 +10,67 @@ using UnitTests.UI.Controllers.TestBases;
 
 namespace UnitTests.UI.Controllers
 {
-    [TestFixture]
-    public class HomeControllerTests : HomeControllerTestBase
-    {
-        [Test]
-        public void ThatIndexActionReturnsAView()
-        {
-            //--Arrange
-            _controller.Get<IAlbumService>()
-                .Expect(x => x.GetAll(Arg<string>.Is.Anything, Arg<string>.Is.Anything, Arg<int>.Is.Anything, Arg<int>.Is.Anything))
-                .Return(new List<Album> { new Album() });
-            _controller.Get<IBookService>()
-                .Expect(x => x.GetAll(Arg<string>.Is.Anything, Arg<string>.Is.Anything, Arg<int>.Is.Anything, Arg<int>.Is.Anything))
-                .Return(new List<Book>());
-            _controller.Get<IMovieService>()
-                .Expect(x => x.GetAll(Arg<string>.Is.Anything, Arg<string>.Is.Anything, Arg<int>.Is.Anything, Arg<int>.Is.Anything))
-                .Return(new List<Movie>());
-            _controller.Get<IGameService>()
-                .Expect(x => x.GetAll(Arg<string>.Is.Anything, Arg<string>.Is.Anything, Arg<int>.Is.Anything, Arg<int>.Is.Anything))
-                .Return(new List<Game>());
+	[TestFixture]
+	public class HomeControllerTests : HomeControllerTestBase
+	{
+		[Test]
+		public void ThatIndexActionReturnsAView()
+		{
+			//--Arrange
+			_controller.Get<IAlbumService>()
+				.Expect(x => x.GetAll(Arg<string>.Is.Anything, Arg<string>.Is.Anything, Arg<int>.Is.Anything, Arg<int>.Is.Anything))
+				.Return(new List<Album> { new Album() });
+			_controller.Get<IBookService>()
+				.Expect(x => x.GetAll(Arg<string>.Is.Anything, Arg<string>.Is.Anything, Arg<int>.Is.Anything, Arg<int>.Is.Anything))
+				.Return(new List<Book>());
+			_controller.Get<IMovieService>()
+				.Expect(x => x.GetAll(Arg<string>.Is.Anything, Arg<string>.Is.Anything, Arg<int>.Is.Anything, Arg<int>.Is.Anything))
+				.Return(new List<Movie>());
+			_controller.Get<IGameService>()
+				.Expect(x => x.GetAll(Arg<string>.Is.Anything, Arg<string>.Is.Anything, Arg<int>.Is.Anything, Arg<int>.Is.Anything))
+				.Return(new List<Game>());
+			_controller.Get<IPopService>()
+				.Expect(x => x.GetAll(Arg<string>.Is.Anything, Arg<string>.Is.Anything, Arg<int>.Is.Anything, Arg<int>.Is.Anything))
+				.Return(new List<FunkoModel>());
 
-            //--Act
-            var result = _controller.ClassUnderTest.Index() as ViewResult;
+			//--Act
+			var result = _controller.ClassUnderTest.Index() as ViewResult;
 
-            // Assert
-            Assert.AreEqual(string.Empty, result.ViewName);
-        }
+			// Assert
+			Assert.AreEqual(string.Empty, result.ViewName);
+		}
 
-        [Test]
-        public void ThatAboutActionReturnsAView()
-        {
-            //--Act
-            var result = _controller.ClassUnderTest.About() as ViewResult;
+		[Test]
+		public void ThatAboutActionReturnsAView()
+		{
+			//--Act
+			var result = _controller.ClassUnderTest.About() as ViewResult;
 
-            //--Assert
-            Assert.AreEqual(string.Empty, result.ViewName);
-        }
+			//--Assert
+			Assert.AreEqual(string.Empty, result.ViewName);
+		}
 
-        [Test]
-        public void ThatContactActionReturnsAView()
-        {
-            //--Act
-            var result = _controller.ClassUnderTest.Contact() as ViewResult;
+		[Test]
+		public void ThatContactActionReturnsAView()
+		{
+			//--Act
+			var result = _controller.ClassUnderTest.Contact() as ViewResult;
 
-            //--Assert
-            Assert.AreEqual(string.Empty, result.ViewName);
-        }
+			//--Assert
+			Assert.AreEqual(string.Empty, result.ViewName);
+		}
 
-        [Test]
-        [TestCase("Metallica", ItemType.Album, "Index")]
-        [TestCase("Killing Joke", ItemType.Book, "Search")]
-        [TestCase("Notebook", ItemType.Movie, "Create")]
-        [TestCase("Until Dawn", ItemType.Game, "Index")]
-        public void ThatSearchActionReturnsCorrectPage(string query, ItemType type, string act)
-        {
-            var result = _controller.ClassUnderTest.Search(query, type, act) as RedirectToRouteResult;
+		[Test]
+		[TestCase("Metallica", ItemType.Album, "Index")]
+		[TestCase("Killing Joke", ItemType.Book, "Search")]
+		[TestCase("Notebook", ItemType.Movie, "Create")]
+		[TestCase("Until Dawn", ItemType.Game, "Index")]
+		public void ThatSearchActionReturnsCorrectPage(string query, ItemType type, string act)
+		{
+			var result = _controller.ClassUnderTest.Search(query, type, act) as RedirectToRouteResult;
 
-            result.RouteValues["Action"].ShouldBe(act);
-            result.RouteValues["Controller"].ShouldBe(type.ToString());
-        }
-    }
+			result.RouteValues["Action"].ShouldBe(act);
+			result.RouteValues["Controller"].ShouldBe(type.ToString());
+		}
+	}
 }
--- UnitTests/UI/Controllers/ShowcaseControllerTests.cs
+++ UnitTests/UI/Controllers/ShowcaseControllerTests.cs
@@ -1,57 +1,61 @@
-﻿using System.Collections.Generic;
-using BusinessLogic.Enums;
-using NUnit.Framework;
-using Shouldly;
-using System.Web.Mvc;
+﻿using BusinessLogic.Enums;
 using BusinessLogic.Models;
 using BusinessLogic.Services.Interfaces;
+using NUnit.Framework;
 using Rhino.Mocks;
+using Shouldly;
+using System.Collections.Generic;
+using System.Web.Mvc;
 using UnitTests.UI.Controllers.TestBases;
 
 namespace UnitTests.UI.Controllers
 {
-    public class ShowcaseControllerTests : ShowcaseControllerTestBase
-    {
-        [Test]
-        public void ThatIndexActionReturnsAView()
-        {
-            _controller.Get<IAlbumService>()
-                .Expect(x => x.GetAll(Arg<string>.Is.Anything, Arg<string>.Is.Anything, Arg<int>.Is.Anything, Arg<int?>.Is.Anything))
-                .Return(new List<Album>());
-            _controller.Get<IBookService>()
-                .Expect(x => x.GetAll(Arg<string>.Is.Anything, Arg<string>.Is.Anything, Arg<int>.Is.Anything, Arg<int?>.Is.Anything))
-                .Return(new List<Book>());
-            _controller.Get<IGameService>()
-                .Expect(x => x.GetAll(Arg<string>.Is.Anything, Arg<string>.Is.Anything, Arg<int>.Is.Anything, Arg<int?>.Is.Anything))
-                .Return(new List<Game>());
-            _controller.Get<IMovieService>()
-                .Expect(x => x.GetAll(Arg<string>.Is.Anything, Arg<string>.Is.Anything, Arg<int>.Is.Anything, Arg<int?>.Is.Anything))
-                .Return(new List<Movie>());
+	public class ShowcaseControllerTests : ShowcaseControllerTestBase
+	{
+		[Test]
+		public void ThatIndexActionReturnsAView()
+		{
+			_controller.Get<IAlbumService>()
+				.Expect(x => x.GetAll(Arg<string>.Is.Anything, Arg<string>.Is.Anything, Arg<int>.Is.Anything, Arg<int?>.Is.Anything))
+				.Return(new List<Album>());
+			_controller.Get<IBookService>()
+				.Expect(x => x.GetAll(Arg<string>.Is.Anything, Arg<string>.Is.Anything, Arg<int>.Is.Anything, Arg<int?>.Is.Anything))
+				.Return(new List<Book>());
+			_controller.Get<IGameService>()
+				.Expect(x => x.GetAll(Arg<string>.Is.Anything, Arg<string>.Is.Anything, Arg<int>.Is.Anything, Arg<int?>.Is.Anything))
+				.Return(new List<Game>());
+			_controller.Get<IMovieService>()
+				.Expect(x => x.GetAll(Arg<string>.Is.Anything, Arg<string>.Is.Anything, Arg<int>.Is.Anything, Arg<int?>.Is.Anything))
+				.Return(new List<Movie>());
+			_controller.Get<IPopService>()
+				.Expect(x => x.GetAll(Arg<string>.Is.Anything, Arg<string>.Is.Anything, Arg<int>.Is.Anything, Arg<int?>.Is.Anything))
+				.Return(new List<FunkoModel>());
 
-            var result = _controller.ClassUnderTest.Index() as ViewResult;
+			var result = _controller.ClassUnderTest.Index() as ViewResult;
 
-            string.IsNullOrWhiteSpace(result.ViewName).ShouldBeTrue();
-        }
+			string.IsNullOrWhiteSpace(result.ViewName).ShouldBeTrue();
+		}
 
-        [Test]
-        public void ThatAddActionReturnsAView()
-        {
-            var result = _controller.ClassUnderTest.Add() as ViewResult;
+		[Test]
+		public void ThatAddActionReturnsAView()
+		{
+			var result = _controller.ClassUnderTest.Add() as ViewResult;
 
-            string.IsNullOrWhiteSpace(result.ViewName).ShouldBeTrue();
-        }
+			string.IsNullOrWhiteSpace(result.ViewName).ShouldBeTrue();
+		}
 
-        [Test]
-        [TestCase(ItemType.Album)]
-        [TestCase(ItemType.Book)]
-        [TestCase(ItemType.Game)]
-        [TestCase(ItemType.Movie)]
-        public void ThatSearchRedirectsToCorrectController(ItemType type)
-        {
-            var result = _controller.ClassUnderTest.SearchItems("test", type) as RedirectToRouteResult;
+		[Test]
+		[TestCase(ItemType.Album)]
+		[TestCase(ItemType.Book)]
+		[TestCase(ItemType.Game)]
+		[TestCase(ItemType.Movie)]
+		[TestCase(ItemType.Pop)]
+		public void ThatSearchRedirectsToCorrectController(ItemType type)
+		{
+			var result = _controller.ClassUnderTest.SearchItems("test", type) as RedirectToRouteResult;
 
-            result.RouteValues["Controller"].ShouldBe(type.ToString());
-            result.RouteValues["Action"].ShouldBe("Index");
-        }
-    }
+			result.RouteValues["Controller"].ShouldBe(type.ToString());
+			result.RouteValues["Action"].ShouldBe("Index");
+		}
+	}
 }
