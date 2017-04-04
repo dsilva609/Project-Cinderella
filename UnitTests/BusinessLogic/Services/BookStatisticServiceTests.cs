cf18d14eae0b96b18dd4b9a7ccd4bc4a4a26943e
Dennis D. Silva <dsilva609@gmail.com>
2017-04-04 13:11:27 -0400

    started implementing book stat service

---
+++ UnitTests/BusinessLogic/Services/BookStatisticServiceTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using UnitTests.BusinessLogic.Services.TestBases;
+
+namespace UnitTests.BusinessLogic.Services
+{
+    public class BookStatisticServiceTests : BookStatisticServiceTestBase
+    {
+        [Test]
+        public void ItGetsNumNovels() => _service.ClassUnderTest.NumNovel().ShouldBe(1);
+    }
+}
---
+++ UnitTests/BusinessLogic/Services/TestBases/BookStatisticServiceTestBase.cs
@@ -0,0 +1,34 @@
+﻿using BusinessLogic.Models;
+using BusinessLogic.Services;
+using BusinessLogic.Services.Statistics;
+using NUnit.Framework;
+using Rhino.Mocks;
+using StructureMap.AutoMocking;
+using System.Collections.Generic;
+
+namespace UnitTests.BusinessLogic.Services.TestBases
+{
+    public class BookStatisticServiceTestBase
+    {
+        protected RhinoAutoMocker<BookStatisticService> _service;
+
+        [SetUp]
+        public virtual void SetUp()
+        {
+            _service = new RhinoAutoMocker<BookStatisticService>();
+
+            _service.Get<BookService>().Expect(x => x.GetAll()).Return(new List<Book>
+            {
+                new Book
+                {
+                },
+                new Book
+                {
+                },
+                new Book
+                {
+                }
+            });
+        }
+    }
+}
