be94f2b0fff74f95b01995fb5ebf15e6527846a1
dsilva609 <dsilva609@gmail.com>
2017-04-14 19:39:48 -0400

    added comment for pending removal

--- BusinessLogic/Models/StatisticsModels/CollectionStatistic.cs
+++ BusinessLogic/Models/StatisticsModels/CollectionStatistic.cs
@@ -1,16 +1,17 @@
 ﻿namespace BusinessLogic.Models.StatisticsModels
 {
-    public class CollectionStatistic
-    {
-        public int CollectionCount { get; set; }
-        public int NumNew { get; set; }
-        public int NumUsed { get; set; }
-        public int NumPhysical { get; set; }
-        public int NumDigital { get; set; }
-        public int TimesCompleted { get; set; }
-        public int NumCompleted { get; set; }
-        public int NumInProgress { get; set; }
-        public int NumNotStarted { get; set; }
-        public int NumCheckedOut { get; set; }
-    }
+	//TODO: remove?
+	public class CollectionStatistic
+	{
+		public int CollectionCount { get; set; }
+		public int NumNew { get; set; }
+		public int NumUsed { get; set; }
+		public int NumPhysical { get; set; }
+		public int NumDigital { get; set; }
+		public int TimesCompleted { get; set; }
+		public int NumCompleted { get; set; }
+		public int NumInProgress { get; set; }
+		public int NumNotStarted { get; set; }
+		public int NumCheckedOut { get; set; }
+	}
 }
--- BusinessLogic/Models/StatisticsModels/GlobalStats.cs
+++ BusinessLogic/Models/StatisticsModels/GlobalStats.cs
@@ -1,10 +1,11 @@
 ﻿namespace BusinessLogic.Models.StatisticsModels
 {
-    public class GlobalStats : CollectionStatistic
-    {
-        public int NumAlbums { get; set; }
-        public int NumBooks { get; set; }
-        public int NumMoviesAndShows { get; set; }
-        public int NumGames { get; set; }
-    }
+	//TODO: remove?
+	public class GlobalStats : CollectionStatistic
+	{
+		public int NumAlbums { get; set; }
+		public int NumBooks { get; set; }
+		public int NumMoviesAndShows { get; set; }
+		public int NumGames { get; set; }
+	}
 }
