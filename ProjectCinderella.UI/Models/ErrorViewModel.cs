a3241b70db4718e5d5f3b17dc944cab552d1a0dc
Dennis D. Silva <dsilva609@gmail.com>
2017-06-12 13:00:40 -0400

    fixed build and added new UI project

---
+++ ProjectCinderella.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjectCinderella.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
